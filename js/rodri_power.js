03b867447f9f9d1a52770bcb780c9a269ab6b0d8
RodrigoBenevenutti <benevenuttirodrigo82@gmail.com>
2020-10-21 16:32:12 +0200

    iframe

---
+++ js/rodri_power.js
@@ -0,0 +1,6 @@
+function loadPage(){
+    var frame = $('#frame');
+    var url = 'https://www.recetasgratis.net/Recetas-de-Arroces-cereales-listado_receta-9_1.html';
+    frame.attr('src',url).show();
+}
+
